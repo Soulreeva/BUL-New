2444176b37ddb75bfb97823ad633ed9af9fff108
Christopher Cardwell <christopher.cardwell@randox.com>
2023-03-30 10:27:13 +0100

    Adding firebase init

---
+++ BUL-NEW/Presentation/BUL-New/src/environments/environments.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAoMP-lejvVa9nNXd55Ck2oKTn3VvR9fEQ',
+    authDomain: 'belfasturbanlogistics.firebaseapp.com',
+    projectId: 'belfasturbanlogistics',
+    storageBucket: 'belfasturbanlogistics.appspot.com',
+    messagingSenderId: '578897052007',
+    appId: '1:578897052007:web:9d0fcba5c0f20172cdafc2',
+    measurementId: 'G-5HLQ85B4SW',
+  },
+};
