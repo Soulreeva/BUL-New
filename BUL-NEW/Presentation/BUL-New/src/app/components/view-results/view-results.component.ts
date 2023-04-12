9e7aeed6d5359587fc7243cf38bf9fa22f2350a1
Chris Cardwell <chris_cardwell89@hotmail.co.uk>
2023-04-13 00:57:18 +0100

    trying to fix db issues

--- BUL-NEW/Presentation/BUL-New/src/app/components/view-results/view-results.component.ts
+++ BUL-NEW/Presentation/BUL-New/src/app/components/view-results/view-results.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Breach } from 'src/app/models/breach';
+import { Paste } from 'src/app/models/paste';
 import { BreachService } from 'src/app/services/breach/breach.service';
 import { PasswordService } from 'src/app/services/password/password.service';
 import { PasteService } from 'src/app/services/paste/paste.service';
@@ -9,26 +11,12 @@ import { PasteService } from 'src/app/services/paste/paste.service';
   styleUrls: ['./view-results.component.scss'],
 })
 export class ViewResultsComponent {
-  public passwordResults?: any;
-  public pasteResults?: any;
-  public breachResults?: any;
-
-  public passwordColumns: string[] = ['index', 'password', 'count', 'dateCreated'];
-
-  public pasteColumns: string[] = ['index', 'search', 'searchDate', 'id', 'source', 'title', 'emailCount', 'date'];
-
-  public breachColumns?: string[] = [
-    'index',
-    'search',
-    'searchDate',
-    'name',
-    'title',
-    'domain',
-    'breachDate',
-    'addedDate',
-    'modifiedDate',
-    'pwnCount',
-  ];
+  public passwordResults: any[] = [];
+  public pasteResults: Paste[] = [];
+  public breachResults: Breach[] = [];
+  public passwordColumns: string[] = [];
+  public pasteColumns: string[] = [];
+  public breachColumns: string[] = [];
 
   constructor(
     private breachService: BreachService,
@@ -42,15 +30,34 @@ export class ViewResultsComponent {
     this.getPasteData();
   }
 
-  private getBreachData() {}
+  private getBreachData() {
+    this.breachColumns = [
+      'index',
+      'search',
+      'searchDate',
+      'name',
+      'title',
+      'domain',
+      'breachDate',
+      'addedDate',
+      'modifiedDate',
+      'pwnCount',
+    ];
+  }
 
   private getPasswordData() {
-    this.passwordResults = this.passwordService.getPasswordFromDb();
-
-    var passwords = [];
+    this.passwordColumns = ['index', 'password', 'count', 'dateCreated'];
+    this.passwordService
+      .getPasswordFromDb()
+      .then((result: any) => {
+        this.passwordResults = result;
+      })
+      .catch((err: any) => {
+        console.error(err);
+      });
   }
 
   private getPasteData() {
-    this.pasteResults = this.pasteService.getPasteDataFromDb();
+    this.pasteColumns = ['index', 'search', 'searchDate', 'id', 'source', 'title', 'emailCount', 'date'];
   }
 }
